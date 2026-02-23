7a22f87bdca3c42f914a9d6d5ebd2cc979fdd8b3
黄永辉 <760732250@qq.com>
2026-02-23 12:36:50 +0800

    1

--- charge.php
+++ charge.php
@@ -6,7 +6,7 @@
 
 //2016-03-25 新增fttps
 function curl_get_https($url, $data=array(), $header=array(), $timeout=30){ //123
-	$ch = curl_init(); 
+	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // 从证书中检查SSL加密算法是否存在 
 	curl_setopt($ch, CURLOPT_URL, $url); 
